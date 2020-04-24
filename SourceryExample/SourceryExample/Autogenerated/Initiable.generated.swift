d60a0ebab732020016a9cb6ecbb9ec5fa462a9e4
Fred Franco <frederico.calhau@zup.com.br>
2020-04-24 19:47:22 -0300

    refactor template for Initiable to improve readability and maintenance

--- SourceryExample/SourceryExample/Autogenerated/Initiable.generated.swift
+++ SourceryExample/SourceryExample/Autogenerated/Initiable.generated.swift
@@ -2,24 +2,39 @@
 // DO NOT EDIT
 
 extension ChildComponent1 {
-    public init(_ attributeOne: String, attributeTwo: Float? = nil, parentProperties: ParentComponent = ParentComponent()    ) {
+	public init(
+    	_ attributeOne: String,
+		attributeTwo: Float? = nil,
+		parentProperties: ParentComponent = ParentComponent()
+    ) {
         self.attributeOne = attributeOne
         self.attributeTwo = attributeTwo
         self.parentProperties = parentProperties
     }
 }
+
 extension ChildComponent2 {
-    public init(_ attributeThree: String, attributeFour: Float, parentProperties: ParentComponent = ParentComponent()    ) {
+	public init(
+    	_ attributeThree: String,
+		attributeFour: Float,
+		parentProperties: ParentComponent = ParentComponent()
+    ) {
         self.attributeThree = attributeThree
         self.attributeFour = attributeFour
         self.parentProperties = parentProperties
     }
 }
+
 extension ParentComponent {
-    public init(_ first: String? = nil, second: String? = nil, third: Int? = nil    ) {
+	public init(
+    	_ first: String? = nil,
+		second: String? = nil,
+		third: Int? = nil
+    ) {
         self.first = first
         self.second = second
         self.third = third
     }
 }
+
 
