79f97d683aa1b56ecb29c8f1765c506d76128f14
Fred Franco <frederico.calhau@zup.com.br>
2020-04-24 20:07:29 -0300

    make generated init inside the actual type declaration

--- SourceryExample/SourceryExample/Models/Models.swift
+++ SourceryExample/SourceryExample/Models/Models.swift
@@ -25,18 +25,53 @@ public struct ParentComponent: AutoEquatable, AutoInitiable {
         self.second = try container.decodeIfPresent(String.self, forKey: .second)
         self.third = try container.decodeIfPresent(Int.self, forKey: .third)
     }
+
+// sourcery:inline:auto:ParentComponent.Init
+	public init(
+    	first: String? = nil,
+		second: String? = nil,
+		third: Int? = nil
+    ) {
+        self.first = first
+        self.second = second
+        self.third = third
+    }
+// sourcery:end
 }
 
 public struct ChildComponent1: ParentProperties, AutoEquatable, AutoDecodable, AutoInitiable {
     let attributeOne: String
     let attributeTwo: Float?
     public var parentProperties: ParentComponent
+
+// sourcery:inline:auto:ChildComponent1.Init
+	public init(
+    	attributeOne: String,
+		attributeTwo: Float? = nil,
+		parentProperties: ParentComponent = ParentComponent()
+    ) {
+        self.attributeOne = attributeOne
+        self.attributeTwo = attributeTwo
+        self.parentProperties = parentProperties
+    }
+// sourcery:end
 }
 
 public struct ChildComponent2: ParentProperties, AutoEquatable, AutoDecodable, AutoInitiable {
-    
+
     let attributeThree: String
     let attributeFour: Float
     public var parentProperties: ParentComponent
-    
+
+// sourcery:inline:auto:ChildComponent2.Init
+	public init(
+    	attributeThree: String,
+		attributeFour: Float,
+		parentProperties: ParentComponent = ParentComponent()
+    ) {
+        self.attributeThree = attributeThree
+        self.attributeFour = attributeFour
+        self.parentProperties = parentProperties
+    }
+// sourcery:end
 }
