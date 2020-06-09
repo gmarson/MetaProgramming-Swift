6ace3bac9e2bdef2ad5e8519421c0f278378730d
Gabriel Marson <gabrielmarson95@gmail.com>
2020-06-09 14:11:12 -0300

    added third child

--- SourceryExample/SourceryExample/Autogenerated/AutoEquatable.generated.swift
+++ SourceryExample/SourceryExample/Autogenerated/AutoEquatable.generated.swift
@@ -2,33 +2,42 @@
 // DO NOT EDIT
 
 
-// MARK: BaseComponent Equatable
 
+// MARK: BaseComponent Equatable
 extension BaseComponent: Equatable {
      public static func ==(lhs: BaseComponent, rhs: BaseComponent) -> Bool {
-        guard lhs.baseName == rhs.baseName else { return false }
-        guard lhs.baseDescription == rhs.baseDescription else { return false }
-        guard lhs.alignement == rhs.alignement else { return false }
+         guard lhs.baseName == rhs.baseName else { return false }
+         guard lhs.baseDescription == rhs.baseDescription else { return false }
+         guard lhs.alignement == rhs.alignement else { return false }
         return true
     }
 }
-// MARK: ChildComponent1 Equatable
 
+// MARK: ChildComponent1 Equatable
 extension ChildComponent1: Equatable {
      public static func ==(lhs: ChildComponent1, rhs: ChildComponent1) -> Bool {
-        guard lhs.attributeOne == rhs.attributeOne else { return false }
-        guard lhs.attributeTwo == rhs.attributeTwo else { return false }
-        guard lhs.baseProperties == rhs.baseProperties else { return false }
+         guard lhs.attributeOne == rhs.attributeOne else { return false }
+         guard lhs.attributeTwo == rhs.attributeTwo else { return false }
+         guard lhs.baseProperties == rhs.baseProperties else { return false }
         return true
     }
 }
-// MARK: ChildComponent2 Equatable
 
+// MARK: ChildComponent2 Equatable
 extension ChildComponent2: Equatable {
      public static func ==(lhs: ChildComponent2, rhs: ChildComponent2) -> Bool {
-        guard lhs.attributeThree == rhs.attributeThree else { return false }
-        guard lhs.attributeFour == rhs.attributeFour else { return false }
-        guard lhs.baseProperties == rhs.baseProperties else { return false }
+         guard lhs.attributeThree == rhs.attributeThree else { return false }
+         guard lhs.attributeFour == rhs.attributeFour else { return false }
+         guard lhs.baseProperties == rhs.baseProperties else { return false }
+        return true
+    }
+}
+
+// MARK: ChildComponent3 Equatable
+extension ChildComponent3: Equatable {
+     public static func ==(lhs: ChildComponent3, rhs: ChildComponent3) -> Bool {
+         guard lhs.baseProperties == rhs.baseProperties else { return false }
+         guard lhs.importantAttribute == rhs.importantAttribute else { return false }
         return true
     }
 }
--- SourceryExample/SourceryExample/Models/Models.swift
+++ SourceryExample/SourceryExample/Models/Models.swift
@@ -17,16 +17,79 @@ public struct BaseComponent: AutoEquatable, AutoInitiable, AutoDecodable {
     var baseName: String?
     var baseDescription: String?
     var alignement: Alignement = .vertical
+
+// sourcery:inline:auto:BaseComponent.Init
+
+// MARK: - BaseComponent custom init
+	public init(
+		_ baseName: String? = nil,
+		baseDescription: String? = nil,
+		alignement: Alignement = .vertical
+    ) {
+        self.baseName = baseName
+        self.baseDescription = baseDescription
+        self.alignement = alignement
+    }
+// sourcery:end
 }
 
 public struct ChildComponent1: BaseProperties, AutoEquatable, AutoDecodable, AutoInitiable {
     let attributeOne: String
     let attributeTwo: Float?
     public var baseProperties: BaseComponent?
+
+// sourcery:inline:auto:ChildComponent1.Init
+
+// MARK: - ChildComponent1 custom init
+	public init(
+		_ attributeOne: String,
+		attributeTwo: Float? = nil,
+		baseProperties: BaseComponent? = nil
+    ) {
+        self.attributeOne = attributeOne
+        self.attributeTwo = attributeTwo
+        self.baseProperties = baseProperties
+    }
+// sourcery:end
 }
 
 public struct ChildComponent2: BaseProperties, AutoEquatable, AutoDecodable, AutoInitiable {
     let attributeThree: String
     let attributeFour: Float
     public var baseProperties: BaseComponent?
+
+// sourcery:inline:auto:ChildComponent2.Init
+
+// MARK: - ChildComponent2 custom init
+	public init(
+		_ attributeThree: String,
+		attributeFour: Float,
+		baseProperties: BaseComponent? = nil
+    ) {
+        self.attributeThree = attributeThree
+        self.attributeFour = attributeFour
+        self.baseProperties = baseProperties
+    }
+// sourcery:end
+}
+
+public struct ChildComponent3: BaseProperties, AutoEquatable, AutoInitiable {
+    public var baseProperties: BaseComponent?
+    public var importantAttribute: [String: String]
+    // sourcery: skipEquality, skipInitiable
+    public var notImportantAttribute: [String: String]
+
+// sourcery:inline:auto:ChildComponent3.Init
+
+// MARK: - ChildComponent3 custom init
+	public init(
+		_ baseProperties: BaseComponent? = nil,
+		importantAttribute: [String: String],
+		notImportantAttribute: [String: String]
+    ) {
+        self.baseProperties = baseProperties
+        self.importantAttribute = importantAttribute
+        self.notImportantAttribute = notImportantAttribute
+    }
+// sourcery:end
 }
