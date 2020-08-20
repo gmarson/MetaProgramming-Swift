7f7185a73a848734c645750d2f0d4c62248777f5
Gabriel Marson <gabrielmarson95@gmail.com>
2020-08-20 19:29:22 -0300

    migrating to spm

---
+++ SourceryExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SourceryExample
+//
+//  Created by Gabriel Augusto Marson on 20/08/20.
+//  Copyright © 2020 Gabriel Augusto Marson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
